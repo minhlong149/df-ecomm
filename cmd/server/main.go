283bd1fffd7564f3fe9d0de740b6296fd8cad4f1
minhlong149 <minhlong9514@gmail.com>
2023-08-06 17:00:21 +0700

    Implement product handle

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,19 +1,61 @@
 package main
 
 import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+
 	"github.com/gin-gonic/gin"
+
 	"df-ecomm/pkg/handler"
 	"df-ecomm/pkg/service"
 )
 
-func main() {
-	r := gin.Default()
+type App struct {
+	Logger *log.Logger
+	Router *gin.Engine
+}
 
+func (app *App) ProductRouter() {
 	productHandler := handler.ProductHandler{
 		ProductService: &service.ProductService{},
 	}
 
-	r.GET("/products", productHandler.GetAll)
+	productsRoute := app.Router.Group("/products")
+	{
+		productsRoute.GET("/", productHandler.GetAllProducts)
+		productsRoute.POST("/", productHandler.AddNewProduct)
+		productsRoute.PUT("/:id", productHandler.UpdateProductById)
+		productsRoute.DELETE("/:id", productHandler.RemoveProductById)
+	}
+}
 
-	r.Run()
+func main() {
+	app := &App{
+		Logger: log.Default(),
+		Router: gin.Default(),
+	}
+
+	app.ProductRouter()
+
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: app.Router,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != http.ErrServerClosed {
+			app.Logger.Fatalf("Listen: %s", err)
+		}
+	}()
+
+	sigint := make(chan os.Signal, 1)
+	signal.Notify(sigint, os.Interrupt)
+	<-sigint
+
+	if err := srv.Shutdown(context.Background()); err != nil {
+		app.Logger.Fatalf("Server shutdown: %s\n", err)
+	}
 }
--- pkg/handler/product.go
+++ pkg/handler/product.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"strconv"
 	"github.com/gin-gonic/gin"
 	"df-ecomm/pkg/model"
 )
@@ -9,7 +10,63 @@ type ProductHandler struct {
 	ProductService model.ProductService
 }
 
-func (h *ProductHandler) GetAll(c *gin.Context) {
+func (h *ProductHandler) GetAllProducts(c *gin.Context) {
 	products := h.ProductService.GetAll()
 	c.JSON(200, products)
+}
+
+func (h *ProductHandler) AddNewProduct(c *gin.Context) {
+	var newProduct model.NewProduct
+	if err := c.BindJSON(&newProduct); err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+
+	if newProduct.Price < 0 {
+		c.JSON(400, gin.H{"error": "Price must be greater than 0"})
+		return
+	}
+
+	createdProduct := h.ProductService.Create(newProduct)
+	c.JSON(201, createdProduct)
+}
+
+func (h *ProductHandler) UpdateProductById(c *gin.Context) {
+	productId, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(400, gin.H{"error": "Invalid product id"})
+		return
+	}
+
+	var updateProduct model.Product
+	if err := c.BindJSON(&updateProduct); err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+
+	if updateProduct.Price < 0 {
+		c.JSON(400, gin.H{"error": "Price must be greater than 0"})
+		return
+	}
+
+	updatedProduct, err := h.ProductService.UpdateById(productId, updateProduct)
+	if err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(200, updatedProduct)
+}
+
+func (h *ProductHandler) RemoveProductById(c *gin.Context) {
+	productId, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(400, gin.H{"error": "Invalid product id"})
+		return
+	}
+
+	if err := h.ProductService.DeleteById(productId); err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+	c.Status(204)
 }
